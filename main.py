5444b5af1ad38c9fb4eff69289f2f5b90d0930e1
Prasanth Louis <prasanthlouis21@gmail.com>
2024-11-07 20:45:25 -0500

    Adding stop condition

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ GPIO.setmode(GPIO.BCM)
 MOTION_PIN = 14 
 GPIO.setup(MOTION_PIN, GPIO.IN)
 
+TIME_LIMIT = 300
+last_motion_time = time.time()
+
 try:
     print("Motion Detection Started...")
     while True:
         if GPIO.input(MOTION_PIN):
             print("Motion Detected!")
+            last_motion_time = time.time()
             time.sleep(1)  
         else:
             print("No Motion.")
         time.sleep(0.5)  
+
+        if time.time() - last_motion_time > TIME_LIMIT:
+            print("Alert: It seems like th washer and dryer have stopped!")    
+            break
 
 except KeyboardInterrupt:
     print("Program stopped by User")
